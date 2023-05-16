871f94eec0c27a706ff5d92699f90aede61642f7
Marco Polo <seguros.marcopolo@gmail.com>
2023-05-16 17:39:46 -0300

    test:Req02 teste de cobertura

--- app/backend/src/tests/change.me.test.ts
+++ app/backend/src/tests/change.me.test.ts
@@ -4,42 +4,48 @@ import * as chai from 'chai';
 import chaiHttp = require('chai-http');
 
 import { app } from '../app';
-import Example from '../database/models/ExampleModel';
+import TeamModel from '../database/models/Team.model';
+import {getAllTeams, getOneTeam} from '../mock/teams.mock';
 
 import { Response } from 'superagent';
+import TeamService from '../services/Team.services';
 
 chai.use(chaiHttp);
 
 const { expect } = chai;
 
-describe('Seu teste', () => {
-  /**
-   * Exemplo do uso de stubs com tipos
-   */
+describe('Teste para rota /teams', () => {
+  
+  let chaiHttpResponse: Response;
 
-  // let chaiHttpResponse: Response;
+  afterEach(() => {
+    sinon.restore();
+});
 
-  // before(async () => {
-  //   sinon
-  //     .stub(Example, "findOne")
-  //     .resolves({
-  //       ...<Seu mock>
-  //     } as Example);
+describe('GET teams', ()=>{
+  it('Deve retornar todos os times',async () => {
+    sinon.stub(TeamModel, 'findAll').resolves( getAllTeams as TeamModel[]);
+
+    const {status, body}= await
+    chai.request(app).get('/teams')
+    expect(status).to.be.equal(200);
+    expect(body).to.be.deep.equal(getAllTeams);
+  })
+  // it('Deve retornar um time',async () => {
+  //   sinon.stub(TeamModel, 'findByPk').resolves( getOneTeam as TeamModel);
+
+  //   const {status, body}= await
+  //   chai.request(app).get('/teams/1')
+  //   expect(status).to.be.equal(200);
+  //   expect(body).to.be.deep.equal(getOneTeam);
   // });
 
-  // after(()=>{
-  //   (Example.findOne as sinon.SinonStub).restore();
-  // })
+  // it('Deve retornar um erro se não encontrar um time',async () => {
+  //   sinon.stub(TeamModel, 'findByPk').resolves();
 
-  // it('...', async () => {
-  //   chaiHttpResponse = await chai
-  //      .request(app)
-  //      ...
-
-  //   expect(...)
+  //   expect(TeamService.getById(1)).to.be.deep.equal('Não existe time com este id');
+   
   // });
-
-  it('Seu sub-teste', () => {
-    expect(false).to.be.eq(true);
-  });
+})
+  
 });
