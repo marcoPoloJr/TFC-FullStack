6f8e9256926f259860d835b42b9ff3e175224825
Marco Polo <seguros.marcopolo@gmail.com>
2023-05-16 11:20:10 -0300

    feat: Cria migration e model para teams

--- app/backend/src/database/migrations/20230511195826-create-teams.js
+++ app/backend/src/database/migrations/20230511195826-create-teams.js
@@ -18,7 +18,7 @@ module.exports = {
     
   },
 
-  down: async (queryInterface, Sequelize) => {
+  down: async (queryInterface, _Sequelize) => {
      await queryInterface.dropTable('teams');
      
   }
